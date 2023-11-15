2cd33e6a1a3e3f26e1f19938f15e665885e03aee
ThisIsIsmailAhmed <138498903+ThisIsIsmailAhmed@users.noreply.github.com>
2023-11-15 23:12:39 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+const input = document.querySelector('#input')
+const ul = document.querySelector('#ul');
+const form = document.querySelector('.add');
+const itag = document.querySelector('.todos');
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    var li = `<li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${input.value}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>`;
+
+    ul.innerHTML += li
+
+});
+
+itag.addEventListener('click', e => {
+    e.preventDefault();
+    if(e.target.tagName === 'I'){
+        e.target.parentNode.remove()
+    }
+   })
+
+  
+
+
+
 
